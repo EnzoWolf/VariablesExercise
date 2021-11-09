70225d27e2d3208f62a42ddff4e0b873031d4935
EnzoWolf <allen@enzowolf.com>
2021-11-09 15:40:37 -0500

    Variables Exercise v1

---
+++ VariablesExcercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Allocating values to variables
+
+            string dogName = "Ender";
+            int ageOfDog = 8;
+            char nameFirstLetter = 'E';
+            bool firstPet = false;
+            double ageInHumanYrs = 51.5;
+            decimal dogWeight = 44.8m;
+
+            Console.WriteLine($"My dog's name is {dogName}, and I got him {ageOfDog} years ago. The first letter of his name is {nameFirstLetter}.");
+            Console.WriteLine($"I would say he is my first dog, but that would be {firstPet}.");
+            Console.WriteLine($"{dogName}'s true age in human years is {ageInHumanYrs}. Lastly, {dogName} is {dogWeight} lbs.");
+
+        }
+    }
+}
